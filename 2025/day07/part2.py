c57f732ecbe93ad051795bc0b73f7af4fe137c6e
Trey Hunner <trey@treyhunner.com>
2025-12-06 23:14:04 -0800

    Use recursion and caching now

--- 2025/day07/part2.py
+++ 2025/day07/part2.py
@@ -1,5 +1,6 @@
 """Day 07 - Part 1"""
 from collections import Counter
+from functools import cache
 from pathlib import Path
 
 
@@ -7,15 +8,18 @@ def parse_input(filename):
     return (Path(__file__).parent / filename).read_text().splitlines()
 
 
+@cache
+def paths(data, row, column):
+    if row == len(data) - 1:
+        return 1
+    if data[row][column] == "^":
+        return paths(data, row+1, column-1) + paths(data, row+1, column+1)
+    else:
+        return paths(data, row+1, column)
+
+
 def solve(data):
-    possibilities = Counter([data[0].index("S")])
-    for line in data:
-        for index, char in enumerate(line):
-            if char == "^" and index in possibilities:
-                count = possibilities.pop(index)
-                possibilities[index-1] += count
-                possibilities[index+1] += count
-    return possibilities.total()
+    return paths(tuple(data), 0, data[0].index("S"))
 
 
 if __name__ == "__main__":

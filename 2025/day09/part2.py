cdabb2161bbe899163194356ec48633d6d503424
Trey Hunner <trey@treyhunner.com>
2025-12-09 08:53:39 -0800

    Still doesn't work. I think I'm giving up.

--- 2025/day09/part2.py
+++ 2025/day09/part2.py
@@ -1,7 +1,7 @@
 """Day 09 - Part 1"""
 from collections import defaultdict
 from dataclasses import astuple, dataclass
-from itertools import combinations, pairwise
+from itertools import combinations, cycle, pairwise
 from pathlib import Path
 
 
@@ -18,28 +18,57 @@ def parse_input(filename):
 
 
 def area_between(point1, point2):
+    """Return area in square between 2 points."""
     (x1, y1), (x2, y2) = point1, point2
     return (abs((x2 - x1)) + 1) * (abs(y2 - y1) + 1)
 
 
-def segment_range(a, b, exclude=False):
-    a, b = sorted((a, b))
-    if exclude:
-        return range(a+1, b)
+def points_between(point1, point2):
+    """Return points between two points (they share a common coordinate)."""
+    (x1, y1), (x2, y2) = point1, point2
+    if x1 == x2:
+        a, b = sorted((y1, y2))
+        return [Point(x1, y) for y in range(a+1, b)]
     else:
-        return range(a, b+1)
+        a, b = sorted((x1, x2))
+        return [Point(x, y1) for x in range(a+1, b)]
+
+
+def segment_range(a, b):
+    """Return a range between (and including) two numbers."""
+    a, b = sorted((a, b))
+    return range(a, b+1)
+
+
+def is_inside(n, segments):
+    """Return True if n is within the (start, end) segment."""
+    return any(
+        start <= n <= end
+        for start, end in segments
+    )
+
+
+def edges_only(points, axis):
+    """
+    Return only the edge points in a given x/y direction.
+
+    A point is an edge if it is not sandwiched between 2 other points.
+    """
+    edges = [points[0]]
+    for previous, point in pairwise(points):
+        if getattr(point, axis) != getattr(previous, axis) + 1:
+            edges.append(point)
+    if edges[-1] != point:
+        edges.append(point)
+    return edges
 
 
 def group_by(items, *, key):
+    """Group items into a dictionary by a given key funcion."""
     grouped = defaultdict(list)
     for item in items:
         grouped[key(item)].append(item)
     return grouped
-
-
-def minmax(numbers):
-    numbers = list(numbers)  # I'm lazy
-    return (min(numbers), max(numbers))
 
 
 def solve(data):
@@ -48,26 +77,52 @@ def solve(data):
         for line in data
     ]
 
-    x_segments = {}
-    y_segments = {}
+    greens = []
     for p1, p2 in pairwise([*reds, reds[0]]):
-        if p1.x == p2.x:
-            x_segments.setdefault(p1.x, []).append(segment_range(p1.y, p2.y))
-        else:
-            y_segments.setdefault(p1.y, []).append(segment_range(p1.x, p2.x))
+        greens += points_between(p1, p2)
+
+    reds_and_greens = reds + greens
+
+    points_by_x = group_by(reds_and_greens, key=lambda p: p.x)
+    segments_inside_x = {}
+    for x, points in points_by_x.items():
+        points.sort(key=lambda p: p.y)
+        segments = segments_inside_x[x] = []
+        inside = True
+        for p, q in pairwise(edges_only(points, "y")):
+            if inside:
+                segments.append((p.y, q.y))
+            inside = not inside
+
+    points_by_y = group_by(reds_and_greens, key=lambda p: p.y)
+    segments_inside_y = defaultdict(list)
+    for y, points in points_by_y.items():
+        points.sort(key=lambda p: p.x)
+        segments = segments_inside_y[y] = []
+        inside = True
+        for p, q in pairwise(edges_only(points, "x")):
+            if inside:
+                segments.append((p.x, q.x))
+            inside = not inside
+
+    def boundaries_are_inside(a, b):
+        for x in segment_range(a.x, b.x):
+            if not is_inside(a.y, segments_inside_x[x]):
+                return False
+            if not is_inside(b.y, segments_inside_x[x]):
+                return False
+        for y in segment_range(a.y, b.y):
+            if not is_inside(a.x, segments_inside_y[y]):
+                return False
+            if not is_inside(b.x, segments_inside_y[y]):
+                return False
+        return True
 
     biggest_area = 1
     for a, b in combinations(reds, 2):
-        if (area := area_between(a, b)) > biggest_area:
-            for x in segment_range(a.x, b.x, exclude=True):
-                if any(a.y in s or b.y in s for s in x_segments.get(x, [])):
-                    break
-            else:
-                for y in segment_range(a.y, b.y, exclude=True):
-                    if any(a.x in s or b.x in s for s in y_segments.get(y, [])):
-                        break
-                else:
-                    biggest_area = area
+        area = area_between(a, b)
+        if area > biggest_area and boundaries_are_inside(a, b):
+            biggest_area = area
 
     return biggest_area
 

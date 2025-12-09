1d727b4e209c2aa2ed47c277fdad4d279cb7c498
Trey Hunner <trey@treyhunner.com>
2025-12-09 10:48:37 -0800

    Another broken solution

--- 2025/day09/part2.py
+++ 2025/day09/part2.py
@@ -23,52 +23,21 @@ def area_between(point1, point2):
     return (abs((x2 - x1)) + 1) * (abs(y2 - y1) + 1)
 
 
-def points_between(point1, point2):
-    """Return points between two points (they share a common coordinate)."""
-    (x1, y1), (x2, y2) = point1, point2
-    if x1 == x2:
-        a, b = sorted((y1, y2))
-        return [Point(x1, y) for y in range(a+1, b)]
-    else:
-        a, b = sorted((x1, x2))
-        return [Point(x, y1) for x in range(a+1, b)]
-
-
-def segment_range(a, b):
-    """Return a range between (and including) two numbers."""
+def segment_range(a, b, exclusive=False):
+    """Return a range between two numbers."""
     a, b = sorted((a, b))
-    return range(a, b+1)
+    if exclusive:
+        return range(a+1, b)
+    else:
+        return range(a, b+1)
 
 
-def is_inside(n, segments):
+def crosses(n, segments):
     """Return True if n is within the (start, end) segment."""
     return any(
-        start <= n <= end
-        for start, end in segments
+        n in s
+        for s in segments
     )
-
-
-def edges_only(points, axis):
-    """
-    Return only the edge points in a given x/y direction.
-
-    A point is an edge if it is not sandwiched between 2 other points.
-    """
-    edges = [points[0]]
-    for previous, point in pairwise(points):
-        if getattr(point, axis) != getattr(previous, axis) + 1:
-            edges.append(point)
-    if edges[-1] != point:
-        edges.append(point)
-    return edges
-
-
-def group_by(items, *, key):
-    """Group items into a dictionary by a given key funcion."""
-    grouped = defaultdict(list)
-    for item in items:
-        grouped[key(item)].append(item)
-    return grouped
 
 
 def solve(data):
@@ -77,44 +46,24 @@ def solve(data):
         for line in data
     ]
 
-    greens = []
-    for p1, p2 in pairwise([*reds, reds[0]]):
-        greens += points_between(p1, p2)
-
-    reds_and_greens = reds + greens
-
-    points_by_x = group_by(reds_and_greens, key=lambda p: p.x)
-    segments_inside_x = {}
-    for x, points in points_by_x.items():
-        points.sort(key=lambda p: p.y)
-        segments = segments_inside_x[x] = []
-        inside = True
-        for p, q in pairwise(edges_only(points, "y")):
-            if inside:
-                segments.append((p.y, q.y))
-            inside = not inside
-
-    points_by_y = group_by(reds_and_greens, key=lambda p: p.y)
-    segments_inside_y = defaultdict(list)
-    for y, points in points_by_y.items():
-        points.sort(key=lambda p: p.x)
-        segments = segments_inside_y[y] = []
-        inside = True
-        for p, q in pairwise(edges_only(points, "x")):
-            if inside:
-                segments.append((p.x, q.x))
-            inside = not inside
+    x_segments = defaultdict(list)
+    y_segments = defaultdict(list)
+    for a, b in pairwise([*reds, reds[0]]):
+        if a.x == b.x:
+            x_segments[a.x].append(segment_range(a.y, b.y))
+        else:
+            y_segments[a.y].append(segment_range(a.x, b.x))
 
     def boundaries_are_inside(a, b):
-        for x in segment_range(a.x, b.x):
-            if not is_inside(a.y, segments_inside_x[x]):
+        for x in segment_range(a.x, b.x, exclusive=True):
+            if crosses(a.y, x_segments[x]):
                 return False
-            if not is_inside(b.y, segments_inside_x[x]):
+            if crosses(b.y, x_segments[x]):
                 return False
-        for y in segment_range(a.y, b.y):
-            if not is_inside(a.x, segments_inside_y[y]):
+        for y in segment_range(a.y, b.y, exclusive=True):
+            if crosses(a.x, y_segments[y]):
                 return False
-            if not is_inside(b.x, segments_inside_y[y]):
+            if crosses(b.x, y_segments[y]):
                 return False
         return True
 
